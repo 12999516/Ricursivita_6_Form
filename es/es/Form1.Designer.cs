1096fb35fe9318c1f52a5bcb344fcfb0e9b46a68
12999516 <12999516@itisrossi.vi.it>
2025-11-05 22:17:24 +0100

    ended

---
+++ es/es/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿using System.Drawing;
+
+namespace es
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_torre_centro = new System.Windows.Forms.Panel();
+            this.pnl_torre_destra = new System.Windows.Forms.Panel();
+            this.pnl_torre_sinistra = new System.Windows.Forms.Panel();
+            this.pnl_dim_4 = new System.Windows.Forms.Panel();
+            this.pnl_dim_6 = new System.Windows.Forms.Panel();
+            this.pnl_dim_2 = new System.Windows.Forms.Panel();
+            this.pnl_dim_1 = new System.Windows.Forms.Panel();
+            this.pnl_dim_8 = new System.Windows.Forms.Panel();
+            this.pnl_dim_9 = new System.Windows.Forms.Panel();
+            this.pnl_dim_5 = new System.Windows.Forms.Panel();
+            this.pnl_dim_3 = new System.Windows.Forms.Panel();
+            this.lbl_gioco = new System.Windows.Forms.Label();
+            this.btn_avvia = new System.Windows.Forms.Button();
+            this.pnl_dim_7 = new System.Windows.Forms.Panel();
+            this.lbl_ndischi = new System.Windows.Forms.Label();
+            this.nmr_ndischi = new System.Windows.Forms.NumericUpDown();
+            this.pnl_base = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.nmr_ndischi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_torre_centro
+            // 
+            this.pnl_torre_centro.BackColor = System.Drawing.Color.Brown;
+            this.pnl_torre_centro.Location = new System.Drawing.Point(700, 295);
+            this.pnl_torre_centro.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_torre_centro.Name = "pnl_torre_centro";
+            this.pnl_torre_centro.Size = new System.Drawing.Size(30, 251);
+            this.pnl_torre_centro.TabIndex = 0;
+            // 
+            // pnl_torre_destra
+            // 
+            this.pnl_torre_destra.BackColor = System.Drawing.Color.Brown;
+            this.pnl_torre_destra.Location = new System.Drawing.Point(420, 295);
+            this.pnl_torre_destra.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_torre_destra.Name = "pnl_torre_destra";
+            this.pnl_torre_destra.Size = new System.Drawing.Size(30, 251);
+            this.pnl_torre_destra.TabIndex = 0;
+            // 
+            // pnl_torre_sinistra
+            // 
+            this.pnl_torre_sinistra.BackColor = System.Drawing.Color.Brown;
+            this.pnl_torre_sinistra.Location = new System.Drawing.Point(110, 295);
+            this.pnl_torre_sinistra.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_torre_sinistra.Name = "pnl_torre_sinistra";
+            this.pnl_torre_sinistra.Size = new System.Drawing.Size(30, 251);
+            this.pnl_torre_sinistra.TabIndex = 0;
+            // 
+            // pnl_dim_4
+            // 
+            this.pnl_dim_4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pnl_dim_4.Location = new System.Drawing.Point(53, 406);
+            this.pnl_dim_4.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_dim_4.Name = "pnl_dim_4";
+            this.pnl_dim_4.Size = new System.Drawing.Size(170, 25);
+            this.pnl_dim_4.TabIndex = 0;
+            // 
+            // pnl_dim_6
+            // 
+            this.pnl_dim_6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(80)))));
+            this.pnl_dim_6.Location = new System.Drawing.Point(32, 470);
+            this.pnl_dim_6.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_dim_6.Name = "pnl_dim_6";
+            this.pnl_dim_6.Size = new System.Drawing.Size(230, 25);
+            this.pnl_dim_6.TabIndex = 0;
+            // 
+            // pnl_dim_2
+            // 
+            this.pnl_dim_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(255)))), ((int)(((byte)(80)))));
+            this.pnl_dim_2.Location = new System.Drawing.Point(80, 342);
+            this.pnl_dim_2.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_dim_2.Name = "pnl_dim_2";
+            this.pnl_dim_2.Size = new System.Drawing.Size(110, 25);
+            this.pnl_dim_2.TabIndex = 0;
+            // 
+            // pnl_dim_1
+            // 
+            this.pnl_dim_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(80)))), ((int)(((byte)(255)))));
+            this.pnl_dim_1.Location = new System.Drawing.Point(92, 310);
+            this.pnl_dim_1.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_dim_1.Name = "pnl_dim_1";
+            this.pnl_dim_1.Size = new System.Drawing.Size(80, 25);
+            this.pnl_dim_1.TabIndex = 0;
+            // 
+            // pnl_dim_8
+            // 
+            this.pnl_dim_8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(80)))), ((int)(((byte)(120)))));
+            this.pnl_dim_8.Location = new System.Drawing.Point(0, 3);
+            this.pnl_dim_8.Name = "pnl_dim_8";
+            this.pnl_dim_8.Size = new System.Drawing.Size(210, 20);
+            this.pnl_dim_8.TabIndex = 0;
+            // 
+            // pnl_dim_9
+            // 
+            this.pnl_dim_9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(120)))), ((int)(((byte)(255)))));
+            this.pnl_dim_9.Location = new System.Drawing.Point(6, 16);
+            this.pnl_dim_9.Name = "pnl_dim_9";
+            this.pnl_dim_9.Size = new System.Drawing.Size(240, 20);
+            this.pnl_dim_9.TabIndex = 0;
+            // 
+            // pnl_dim_5
+            // 
+            this.pnl_dim_5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(120)))), ((int)(((byte)(255)))));
+            this.pnl_dim_5.Location = new System.Drawing.Point(41, 438);
+            this.pnl_dim_5.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_dim_5.Name = "pnl_dim_5";
+            this.pnl_dim_5.Size = new System.Drawing.Size(200, 25);
+            this.pnl_dim_5.TabIndex = 0;
+            // 
+            // pnl_dim_3
+            // 
+            this.pnl_dim_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(80)))));
+            this.pnl_dim_3.Location = new System.Drawing.Point(68, 374);
+            this.pnl_dim_3.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_dim_3.Name = "pnl_dim_3";
+            this.pnl_dim_3.Size = new System.Drawing.Size(140, 25);
+            this.pnl_dim_3.TabIndex = 0;
+            // 
+            // lbl_gioco
+            // 
+            this.lbl_gioco.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_gioco.Location = new System.Drawing.Point(0, 25);
+            this.lbl_gioco.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_gioco.Name = "lbl_gioco";
+            this.lbl_gioco.Size = new System.Drawing.Size(1067, 62);
+            this.lbl_gioco.TabIndex = 0;
+            this.lbl_gioco.Text = "torri di hanoi";
+            // 
+            // btn_avvia
+            // 
+            this.btn_avvia.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_avvia.Location = new System.Drawing.Point(603, 578);
+            this.btn_avvia.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_avvia.Name = "btn_avvia";
+            this.btn_avvia.Size = new System.Drawing.Size(160, 43);
+            this.btn_avvia.TabIndex = 1;
+            this.btn_avvia.Text = "avvia gioco";
+            this.btn_avvia.UseVisualStyleBackColor = true;
+            this.btn_avvia.Click += new System.EventHandler(this.btn_avvia_Click);
+            // 
+            // pnl_dim_7
+            // 
+            this.pnl_dim_7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(80)))), ((int)(((byte)(255)))));
+            this.pnl_dim_7.Location = new System.Drawing.Point(21, 503);
+            this.pnl_dim_7.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_dim_7.Name = "pnl_dim_7";
+            this.pnl_dim_7.Size = new System.Drawing.Size(270, 25);
+            this.pnl_dim_7.TabIndex = 0;
+            // 
+            // lbl_ndischi
+            // 
+            this.lbl_ndischi.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ndischi.Location = new System.Drawing.Point(16, 584);
+            this.lbl_ndischi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_ndischi.Name = "lbl_ndischi";
+            this.lbl_ndischi.Size = new System.Drawing.Size(348, 37);
+            this.lbl_ndischi.TabIndex = 2;
+            this.lbl_ndischi.Text = "metti il numero di dischi stronzo";
+            // 
+            // nmr_ndischi
+            // 
+            this.nmr_ndischi.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nmr_ndischi.Location = new System.Drawing.Point(420, 582);
+            this.nmr_ndischi.Margin = new System.Windows.Forms.Padding(4);
+            this.nmr_ndischi.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.nmr_ndischi.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nmr_ndischi.Name = "nmr_ndischi";
+            this.nmr_ndischi.Size = new System.Drawing.Size(160, 35);
+            this.nmr_ndischi.TabIndex = 3;
+            this.nmr_ndischi.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // pnl_base
+            // 
+            this.pnl_base.Location = new System.Drawing.Point(12, 527);
+            this.pnl_base.Name = "pnl_base";
+            this.pnl_base.Size = new System.Drawing.Size(736, 23);
+            this.pnl_base.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 641);
+            this.Controls.Add(this.pnl_base);
+            this.Controls.Add(this.nmr_ndischi);
+            this.Controls.Add(this.lbl_ndischi);
+            this.Controls.Add(this.pnl_torre_centro);
+            this.Controls.Add(this.pnl_dim_4);
+            this.Controls.Add(this.pnl_dim_7);
+            this.Controls.Add(this.pnl_dim_6);
+            this.Controls.Add(this.pnl_dim_2);
+            this.Controls.Add(this.pnl_dim_5);
+            this.Controls.Add(this.pnl_dim_1);
+            this.Controls.Add(this.pnl_torre_destra);
+            this.Controls.Add(this.pnl_torre_sinistra);
+            this.Controls.Add(this.pnl_dim_3);
+            this.Controls.Add(this.lbl_gioco);
+            this.Controls.Add(this.btn_avvia);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nmr_ndischi)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_torre_centro;
+        private System.Windows.Forms.Panel pnl_torre_destra;
+        private System.Windows.Forms.Panel pnl_torre_sinistra;
+        private System.Windows.Forms.Panel pnl_dim_1;
+        private System.Windows.Forms.Panel pnl_dim_4;
+        private System.Windows.Forms.Panel pnl_dim_2;
+        private System.Windows.Forms.Panel pnl_dim_5;
+        private System.Windows.Forms.Panel pnl_dim_3;
+        private System.Windows.Forms.Panel pnl_dim_6;
+        private System.Windows.Forms.Panel pnl_dim_8;
+        private System.Windows.Forms.Panel pnl_dim_9;
+        private System.Windows.Forms.Button btn_avvia;
+        private System.Windows.Forms.Label lbl_gioco;
+        private System.Windows.Forms.Panel pnl_dim_7;
+        private System.Windows.Forms.Label lbl_ndischi;
+        private System.Windows.Forms.NumericUpDown nmr_ndischi;
+        private System.Windows.Forms.Panel pnl_base;
+    }
+}
+
---
+++ es/es/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;           
+using System.Windows.Forms;     
+
+namespace es                    
+{
+    public partial class Form1 : Form
+    {
+
+        Panel[] dischi;                    // Array che contiene i dischi
+
+        int[] posDischi;                   //posizione di ogni disco 0 = sinistra, 1 = centro, 2 = destra
+
+        int[] torreX;                      // Coordinate X del centro di ogni torre (sinistra, centro, destra)
+
+        int baseY;                         // y dove partono e si devono fermare i dischi
+
+        int nDischi;                       // dischi che si vogliono usare
+
+        (int disco, int from, int to)[] mosse;     //tupla delle mosse
+
+        int indiceMosse;                   // mossa in esecuzione
+
+        Timer timer;                       // oggetto timer per animazione
+
+        int fase = 0;                      // movimento 0 = sale, 1 = orizzontale, 2 = scende
+
+        int targetX, targetY;              // coordinate che il disco deve raggiungere
+
+        int discoCorrente = -1;            // disco che si sta muovendo
+
+        public Form1()
+        {
+            InitializeComponent();             
+
+            
+            dischi = new Panel[]                   // creo un array con i dischi
+            {
+                pnl_dim_1, pnl_dim_2, pnl_dim_3, pnl_dim_4,  
+                pnl_dim_5, pnl_dim_6, pnl_dim_7              
+            };
+
+            posDischi = new int[dischi.Length];    // Creo array per ricordare posizione di ogni disco
+
+
+            torreX = new int[]                     // Salvo il centro X di ogni torre
+            {
+                pnl_torre_sinistra.Left + pnl_torre_sinistra.Width / 2,   // centro torre sinistra
+                pnl_torre_centro.Left   + pnl_torre_centro.Width   / 2,   // centro torre centrale
+                pnl_torre_destra.Left   + pnl_torre_destra.Width   / 2    // centro torre destra
+            };
+
+
+            baseY = (pnl_base != null) ? pnl_base.Top : this.ClientRectangle.Bottom - 40; //assegno il valore y della base
+
+
+            timer = new Timer();                   // Creo un timer
+            timer.Interval = 100;                   // ogni quanto
+            timer.Tick += Timer_Tick;              // Quando scatta, chiama la funzione Timer_Tick
+        }
+
+        
+        private void btn_avvia_Click(object sender, EventArgs e)
+        {
+            nDischi = (int)nmr_ndischi.Value;      // Legge quanti dischi vuole l'utente (dal NumericUpDown)
+
+            for (int i = 0; i < dischi.Length; i++)
+            {
+                dischi[i].Visible = i < nDischi;    // Mostra solo i primi n dischi
+            }
+
+            
+            for (int i = 0; i < nDischi; i++)
+            {
+                posDischi[i] = 0;                  // 0 = torre sinistra
+            }
+
+            
+            for (int i = nDischi - 1; i >= 0; i--)
+            {
+                ImpilaDisco(i, 0);                 // Impila il disco i sulla torre 0 (sinistra)
+            }
+
+            
+            mosse = new (int, int, int)[2048];     // Array grande a sufficienza (max 2047 mosse per 11 dischi)
+            indiceMosse = 0;                       // Partiamo dalla mossa 0
+            GeneraMosse(nDischi, 0, 2, 1);         // Muovi n dischi da torre 0 a torre 2 usando 1 come ausiliaria
+
+            
+            fase = 0;                              // Inizia dalla fase di salita
+            discoCorrente = -1;                    // Nessun disco in movimento
+            timer.Start();                         // Avvia il timer → animazione parte
+        }
+
+        
+        private void ImpilaDisco(int index, int torre)
+        {
+            Panel d = dischi[index];               // Prendo il pannello del disco
+            int altezza = d.Height;                // Altezza del disco (tutti uguali)
+
+            // Conta quanti dischi sono già sulla torre (escludendo questo)
+            int dischiSotto = 0;
+            for (int i = 0; i < nDischi; i++)
+            {
+                if (i != index && posDischi[i] == torre)  // Se è sulla stessa torre e non è questo disco
+                    dischiSotto++;
+            }
+
+            // Calcola Y: base - (dischi sotto + 1) * altezza
+            int y = baseY - (dischiSotto + 1) * altezza;
+            int x = torreX[torre] - d.Width / 2;   // Centra il disco sulla torre
+
+            d.Left = x;                            // Imposta posizione X
+            d.Top = y;                             // Imposta posizione Y
+        }
+
+        
+        private void GeneraMosse(int n, int from, int to, int aux)
+        {
+            if (n == 0) return;                    // Caso base: 0 dischi → niente da fare
+
+            // Passo 1: sposta n-1 dischi da "from" a "aux" (usando "to" come ausiliario)
+            GeneraMosse(n - 1, from, aux, to);
+
+            // Passo 2: sposta il disco più grande (n) da "from" a "to"
+            mosse[indiceMosse++] = (n - 1, from, to);  // n-1 perché indice parte da 0
+
+            // Passo 3: sposta n-1 dischi da "aux" a "to" (usando "from" come ausiliario)
+            GeneraMosse(n - 1, aux, to, from);
+        }
+
+        
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (indiceMosse == 0)                  // Se non ci sono più mosse
+            {
+                timer.Stop();                      // Ferma l'animazione
+                return;
+            }
+
+            // Se non stiamo muovendo nessun disco, prendi la prossima mossa
+            if (fase == 0 && discoCorrente == -1)
+            {
+                discoCorrente = mosse[0].disco;    // Quale disco muovere
+            }
+
+            const int step = 8;                    // Quanti pixel si muove per frame (velocità)
+
+            
+            if (fase == 0)
+            {
+                // Muovi il disco verso l'alto
+                dischi[discoCorrente].Top = Math.Max(dischi[discoCorrente].Top - step, 60);
+
+                // Quando arriva in cima (Y=60), passa alla fase orizzontale
+                if (dischi[discoCorrente].Top <= 60)
+                {
+                    fase = 1;
+                    targetX = torreX[mosse[0].to] - dischi[discoCorrente].Width / 2;
+                }
+            }
+
+            
+            else if (fase == 1)
+            {
+                int left = dischi[discoCorrente].Left;
+
+                // Muovi verso destra o sinistra
+                if (left < targetX)
+                    left = Math.Min(left + step, targetX);
+                else if (left > targetX)
+                    left = Math.Max(left - step, targetX);
+
+                dischi[discoCorrente].Left = left;
+
+                // Quando arriva al centro della torre di destinazione
+                if (left == targetX)
+                {
+                    fase = 2;
+                    int sotto = ContaDischiTorre(mosse[0].to);  // Quanti dischi già lì?
+                    targetY = baseY - (sotto + 1) * dischi[discoCorrente].Height;
+                }
+            }
+
+            
+            else if (fase == 2)
+            {
+                dischi[discoCorrente].Top = Math.Min(dischi[discoCorrente].Top + step, targetY);
+
+                // Quando tocca la posizione finale
+                if (dischi[discoCorrente].Top >= targetY)
+                {
+                    dischi[discoCorrente].Top = targetY;           // Forza posizione esatta
+                    posDischi[discoCorrente] = mosse[0].to;        // Aggiorna posizione logica
+
+                    // Rimuovi la mossa completata dall'array
+                    Array.Copy(mosse, 1, mosse, 0, --indiceMosse);
+
+                    // Prepara la prossima mossa
+                    fase = 0;
+                    discoCorrente = -1;
+                }
+            }
+        }
+
+        
+        private int ContaDischiTorre(int torre)
+        {
+            int count = 0;
+            for (int i = 0; i < nDischi; i++)
+            {
+                // Se il disco è sulla torre E non è quello che stiamo muovendo
+                if (i != discoCorrente && posDischi[i] == torre)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
